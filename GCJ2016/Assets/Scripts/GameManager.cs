37462b2f91ba9be4eeaa4241a030f660b5c51c63
Fernand <fernand.veyrier@epitech.eu>
2016-05-07 15:31:39 +0200

    menu working

--- GCJ2016/Assets/Scripts/GameManager.cs
+++ GCJ2016/Assets/Scripts/GameManager.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
-	
+
+	public const float gravityEarth = 9.81f;
+	public const float gravityMoon = 1.6f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +15,20 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void LoadLevel(int level)
+	{
+		SceneManager.LoadSceneAsync (level);
+	}
+
+	public void ReloadLevel()
+	{
+		SceneManager.LoadSceneAsync (SceneManager.GetActiveScene ().buildIndex);
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
 	}
 }
